b135ae306a5c9fa2691e733886e77b314b439edd
Ben Myers <ben.myers@okstate.edu>
2019-06-18 19:08:05 -0500

    Boilerplate!

---
+++ README.md
@@ -0,0 +1,13 @@
+# Simple Ink Starter
+
+> A nearly minimal viable repository to use as a template for creating new [Ink](https://www.npmjs.com/package/ink) applications
+
+
+## How to Use the Starter
+
+```bash
+git clone https://github.com/BenDMyers/Simple_Ink_Starter.git
+cd Simple_Ink_Starter
+yarn install
+npm start
+```
---
+++ cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+'use strict';
+
+const importJsx = require('import-jsx');
+const {h, render} = require('ink');
+const meow = require('meow');
+
+const Ui = importJsx('./App');
+
+const cli = meow(`
+	Usage
+	  $ ink-test [input]
+
+	Options
+	  --name  Lorem ipsum [Default: false]
+
+	Examples
+	  $ ink-test
+	  I love Ink
+	  $ ink-test --name=ponies
+	  I love ponies
+`);
+
+render(h(Ui, cli.flags));
